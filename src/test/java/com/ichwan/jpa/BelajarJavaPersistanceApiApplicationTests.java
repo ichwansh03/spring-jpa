c38a28ef3831b7b8cf12f1ca972a5510dffb13f5
ichwansh03 <ichwansholihin03@gmail.com>
2023-10-17 11:04:55 +0700

    setup project jpa

---
+++ src/test/java/com/ichwan/jpa/BelajarJavaPersistanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ichwan.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaPersistanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
