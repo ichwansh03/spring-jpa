fc805e3e18912da8bae8b025099a09d086ca7f53
ichwansh03 <ichwansholihin03@gmail.com>
2023-10-18 16:23:31 +0700

    chore: add generated value for auto_inc

---
+++ src/main/java/com/ichwan/jpa/entity/Category.java
@@ -0,0 +1,22 @@
+package com.ichwan.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String description;
+}
