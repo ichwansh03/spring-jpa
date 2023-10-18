4e9dc084d02e9a035beb19588a73ca99d4351ee8
ichwansh03 <ichwansholihin03@gmail.com>
2023-10-18 08:07:38 +0700

    chore: add crud operation jpa

---
+++ src/test/java/com/ichwan/jpa/CrudTest.java
@@ -0,0 +1,85 @@
+package com.ichwan.jpa;
+
+import com.ichwan.jpa.entity.Customer;
+import com.ichwan.jpa.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CrudTest {
+    
+    private EntityManagerFactory entityManagerFactory;
+
+    @BeforeEach
+    void setUp() {
+        entityManagerFactory = JpaUtil.getEntityManagerFactory();
+    }
+
+    @Test
+    void insertData() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Customer customer = new Customer();
+        customer.setId("1");
+        customer.setName("Ichwan");
+
+        Assertions.assertNotNull(customer);
+        entityManager.persist(customer);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void findData() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Customer customer = entityManager.find(Customer.class, "1");
+        Assertions.assertNotNull(customer);
+        Assertions.assertEquals("1", customer.getId());
+        Assertions.assertEquals("Ichwan", customer.getName());
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void updateData() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Customer customer = entityManager.find(Customer.class, "1");
+        customer.setName("Ichwan Sholihin");
+        entityManager.merge(customer);
+
+        Assertions.assertEquals("1", customer.getId());
+        Assertions.assertEquals("Ichwan Sholihin", customer.getName());
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void deleteData() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Customer customer = entityManager.find(Customer.class, "1");
+        entityManager.remove(customer);
+
+        Assertions.assertNull(customer);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+}
